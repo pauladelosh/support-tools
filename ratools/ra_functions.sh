8aaa4b9bed677902e2ec31da5e899c380a98366a
Matt Lavoie <matthew.lavoie@acquia.com>
2014-05-14 10:36:21 -0400

    Adding RATOOLS_VERSION variable and ra-version command.

--- ratools/ra_functions.sh
+++ ratools/ra_functions.sh
@@ -51,6 +51,13 @@
 #
 ############################################################################################
 
+# Current date and build of tools. increment build number by one. format: "build zzzz (yyyy-mm-dd)"
+# DON'T FORGET TO UPDATE THIS WHEN PUSHING TO MASTER!!
+RATOOLS_VERSION="Build 0001 (2014-05-14)"
+
+# Output date and build of current toolset
+alias ra-version='echo $RATOOLS_VERSION'
+
 # Help
 alias ra-help='ratools-help'
 function ratools-help {
