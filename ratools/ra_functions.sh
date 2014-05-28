4a579ef8e0096d94a8b6d3432706c747db892ef4
Samuel Mortenson <samuel.mortenson@acquia.com>
2014-05-28 10:55:01 -0700

    Updated RATOOLS_VERSION to 0005.

--- ratools/ra_functions.sh
+++ ratools/ra_functions.sh
@@ -59,7 +59,7 @@
 
 # Current date and build of tools. increment build number by one. format: "build zzzz (yyyy-mm-dd)"
 # DON'T FORGET TO UPDATE THIS WHEN PUSHING TO MASTER!!
-RATOOLS_VERSION="Build 0004 (2014-05-27)"
+RATOOLS_VERSION="Build 0005 (2014-05-27)"
 
 # Output date and build of current toolset
 alias ra-version='echo $RATOOLS_VERSION'
